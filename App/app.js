824014a82e8da4215299f94f512b6bea68653656
dafnemus <dafnemus@gmail.com>
2021-03-14 13:39:37 -0300

    muestro productos

--- App/app.js
+++ App/app.js
@@ -1,4 +1,8 @@
-const items = document.getElemnetById('items')
+const items = document.getElementById('items')
+const templateItems = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
@@ -8,7 +12,20 @@ const fetchData = async() => {
         const res = await fetch('../Api/api.json')
         const data = await res.json()
         console.log(data)
+        renderItems(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+const renderItems = data => {
+    data.forEach(producto => {
+        templateItems.querySelector('h5').textContent = producto.title
+        templateItems.querySelector('p').textContent = producto.precio
+        templateItems.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+
+        const clone = templateItems.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 }
